0d48ed2d78f0215653f8f24aa4cb426b78258e52
NansiYancheva <106161782+NansiYancheva@users.noreply.github.com>
2023-10-14 15:28:31 +0300

    Add project files.

---
+++ DropDownMenuFlagsDemo/Data/CountryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DropDownMenuFlagsDemo.Data
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+
+        public string Country { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string FlagUrl => $"_content/BlazorFlags/flags/{CountryCode}.png";
+    }
+}
---
+++ DropDownMenuFlagsDemo/Services/CountryService.cs
@@ -0,0 +1,38 @@
+﻿using DropDownMenuFlagsDemo.Data;
+
+
+namespace DropDownMenuFlagsDemo.Services
+{
+    public class CountryService
+    {
+       
+        public static List<CountryDto> GetCountries()
+        {
+            List<CountryDto> countriesList = new List<CountryDto>()
+                {
+                new CountryDto()
+                {
+                    Id = 1,
+                    Country = "Bulgaria",
+                    CountryCode = "BG"
+                },
+                 new CountryDto()
+                 {
+                    Id = 2,
+                    Country = "USA",
+                    CountryCode = "US"
+                 },
+                new CountryDto()
+                {
+                    Id = 3,
+                    Country = "China",
+                    CountryCode = "CN"
+                }
+                };
+
+            return countriesList;
+        }
+    }
+
+
+}
